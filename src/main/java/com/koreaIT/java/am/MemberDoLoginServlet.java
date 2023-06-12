40f4b022966dcab5830fc8fb574020402e336745
shinmj0255 <shinmj0255@gmail.com>
2023-06-12 21:52:03 +0900

    로그아웃 .session 활용

--- src/main/java/com/koreaIT/java/am/MemberDoLoginServlet.java
+++ src/main/java/com/koreaIT/java/am/MemberDoLoginServlet.java
@@ -15,6 +15,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/member/doLogin")
 public class MemberDoLoginServlet extends HttpServlet {
@@ -48,6 +49,9 @@ public class MemberDoLoginServlet extends HttpServlet {
 			if (memberRow.get("loginPw").equals(loginPw) == false) {
 				response.getWriter().append(String.format("<script>alert('비밀번호가 일치하지 않습니다.'); location.replace('login');</script>"));
 			}
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("loginedMemberId", memberRow.get("id"));
 			
 			response.getWriter().append(String.format("<script>alert('%s님 로그인성공'); location.replace('../home/main');</script>", memberRow.get("name")));
 			
--- src/main/webapp/jsp/home/main.jsp
+++ src/main/webapp/jsp/home/main.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<%
+	int loginedMemberId = (int) request.getAttribute("loginedMemberId");
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,8 +13,23 @@
 <body>
 	<h1>메인 페이지</h1>
 	
-	<div><a href="../member/join">회원 가입</a></div>
-	<div><a href="../member/login">로그인</a></div>
+	<%
+		if (loginedMemberId == -1) {
+	%>
+			<div><a href="../member/login">로그인</a></div>
+			<div><a href="../member/join">회원가입</a></div>
+	<% 	
+		}
+	%>
+	
+	<%
+		if (loginedMemberId != -1) {
+	%>
+			<div><a href="../member/doLogout">로그아웃</a></div>
+	<% 	
+		}
+	%>
+	
 	<div><a href="../article/list">게시물 리스트</a></div>
 </body>
 </html>
